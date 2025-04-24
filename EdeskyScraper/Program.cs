25991981fbe6f6a1bf6bcbdf193d70858ce3d8e3
Kocian <kocian.michael@gmail.com>
2025-04-24 23:51:32 +0200

    last edits

--- EdeskyScraper/Program.cs
+++ EdeskyScraper/Program.cs
@@ -2,7 +2,7 @@
 using System.Text.Json;
 using System.Text.RegularExpressions;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("App starting");
 
 // language=regex
 string pattern = @"<tr>.*?javascript:D.'(?<token>.*?)'.*?popis.>\s+(?<popis>.*?)\s+</td>.*?datod.>\s*(?<date>.*?)\s+.*?zdroj.>\s*(?<zdroj>.*?)\s*</td>.*?</tr>";
@@ -11,6 +11,10 @@ string webhookUrl = "https://discord.com/api/webhooks/1365045974917058672/PsJhdk
 
 using var http = new HttpClient();
 string response = await http.GetStringAsync(url);
+if (string.IsNullOrWhiteSpace(response))
+{
+    throw new Exception("unable to download webpage");
+}
 
 RegexOptions options = RegexOptions.IgnoreCase | RegexOptions.Multiline | RegexOptions.Singleline;
 MatchCollection matches = Regex.Matches(response, pattern, options);
@@ -32,7 +36,7 @@ foreach (var e in entries)
     var payload = new
     {
         title = "Nový záznam",
-        content = message,
+        content = "Nový záznam",
         embeds = new List<object>(){new
         {
             author = new {
@@ -44,9 +48,12 @@ foreach (var e in entries)
             url = $"https://egov.opava-city.cz/Uredni_deska/DetailDokument.aspx?IdFile={e.Token}&Por=0",
         } }
     };
-
     var content = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
     var discordresponse = await http.PostAsync(webhookUrl, content);
+    if (!discordresponse.IsSuccessStatusCode)
+    {
+        throw new Exception("discord declined the message");
+    }
     var textresponse = await discordresponse.Content.ReadAsStringAsync();
     await Task.Delay(1000);
 }
